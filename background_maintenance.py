0df0852edd1a7444e7329c49c212fd8c5a145f89
TylerFlar <tulikkovulpus@gmail.com>
2023-02-20 12:34:53 -0800

    Connected video to background model.

--- background_maintenance.py
+++ background_maintenance.py
@@ -3,52 +3,64 @@ from scipy.stats import norm
 import cv2
 
 class AGMM:
-    def __init__(self, N=3, alpha=0.05, T_sigma=2.5, sigma_0=100.0, w_0=0.01):
-        self.N = N
+    def __init__(self, num_gaussians=3, alpha=0.01, T_sigma=2.5, Sigma_0=1e2, w_0=0.01):
+        self.num_gaussians = num_gaussians
         self.alpha = alpha
         self.T_sigma = T_sigma
-        self.sigma_0 = sigma_0
+        self.Sigma_0 = Sigma_0
         self.w_0 = w_0
-        self.mu = np.zeros((N,))
-        self.sigma = np.zeros((N,))
-        self.w = np.zeros((N,))
         
-    def update(self, frame):
-        height, width = frame.shape[:2]
-        model_match = np.zeros((height, width, self.N), dtype=bool)
-        distance = np.zeros((height, width, self.N)) + np.inf
-        
-        # Model matching
-        for n in range(self.N):
-            dist = np.abs(frame - self.mu[n])
-            condition = dist <= self.T_sigma * self.sigma[n]
-            distance[condition, n] = -self.w[n]
-        
-        argmin = np.argmin(distance, axis=2)
-        argmin[distance[argmin, np.arange(argmin.shape[1])] == np.inf] = -1
-        np.putmask(model_match, distance != np.inf, True)
-        # Model renewing
-        self.w = (1 - self.alpha) * self.w + self.alpha * model_match
-        replace_mask = (np.sum(model_match, axis=2) == 0)
-        if np.any(replace_mask):
-            k = np.argmin(self.w)
-            self.mu[k] = frame[replace_mask]
-            self.sigma[k] = self.sigma_0
-            self.w[k] = self.w_0
+        # Initialize variables
+        self.Mu = np.zeros((num_gaussians, 3))
+        self.Sigma = np.zeros((num_gaussians, 3))
+        self.W = np.zeros(num_gaussians)
+        self.frame_count = 0
 
-        update_mask = ~replace_mask
-        if np.any(update_mask):
-            alpha_t = self.alpha / np.sum(model_match[update_mask], axis=2)
-            alpha_t = alpha_t.reshape(-1, self.N)
-            alpha_t[~np.isfinite(alpha_t)] = 0
-            p_t = alpha_t * norm.pdf(frame[update_mask, None], self.mu, self.sigma)
-            p_t[~np.isfinite(p_t)] = 0
-            
-            # Update phase
-            self.mu = (1 - p_t) * self.mu + p_t * frame[update_mask, None]
-            self.sigma = (1 - p_t) * self.sigma + p_t * np.square(frame[update_mask, None] - self.mu)
+
+    def apply(self, frame):
+        # Increment frame count
+        self.frame_count += 1
         
-        self.w = self.w / np.sum(self.w)
+        # Convert frame to float
+        frame = frame.astype(float)
+
+        # Create binary mask
+        mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+
+        # Loop over all pixels
+        for i in range(frame.shape[0]):
+            for j in range(frame.shape[1]):
+                pixel = frame[i, j, :]
+
+                # Check if pixel matches a model
+                match = False
+                for n in range(self.num_gaussians):
+                    if self.W[n] > 0:
+                        if abs(pixel - self.Mu[n]).sum() <= self.T_sigma * np.sqrt(self.Sigma[n]).all():
+                            match = True
+                            self.W[n] = (1 - self.alpha) * self.W[n] + self.alpha
+                            self.Mu[n] = (1 - self.alpha) * self.Mu[n] + self.alpha * pixel
+                            self.Sigma[n] = (1 - self.alpha) * self.Sigma[n] + self.alpha * (pixel - self.Mu[n])**2
+                            break
+                
+                # If no match is found, replace the weakest model
+                if not match:
+                    k = np.argmin(self.W)
+                    self.W[k] = self.w_0
+                    self.Mu[k] = pixel
+                    self.Sigma[k] = self.Sigma_0
+                
+                # Normalize weights
+                self.W /= np.sum(self.W)
+
+                # Create mask
+                if np.max(self.W) >= 0.5:
+                    mask[i, j] = 255
+
+        # Return binary mask
+        return mask
+        
+
 
 # Usage example
 cap = cv2.VideoCapture('videos/car-2165.mp4')
@@ -61,9 +73,12 @@ while cap.isOpened():
         break
 
     # apply AGMM background subtraction to the frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    background = agmm.apply(frame)
 
     # display the foreground mask
-    cv2.imshow('FG Mask', frame)
+    cv2.imshow('Frame', frame)
+    cv2.imshow('background', background)
 
     # check for user input to exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
