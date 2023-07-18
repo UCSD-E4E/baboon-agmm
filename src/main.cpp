eefb2f52f9e2c6d69e88b311abccc79430596ecb
TylerFlar <tulikkovulpus@gmail.com>
2023-07-17 18:05:30 -0700

    lower cores

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ static void glfw_error_callback(int error, const char *description)
 int main(int argc, char **argv)
 {
 #ifdef WITH_OPENMP
-    omp_set_num_threads(8);
+    omp_set_num_threads(4);
 #endif
 
     if (argc < 2)
